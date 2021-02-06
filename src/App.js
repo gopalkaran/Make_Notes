70b9cc4822497a1fd852dc4770445054807d91d8
Gopal Karan <16031024+gopalkaran@users.noreply.github.com>
2021-02-06 23:45:07 +0530

    few modification is done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import "./styles.css";
 import logo from "./description.svg";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function App() {
-  var [title, setTitle] = useState("");
-  var [des, setDes] = useState("");
   var [noteList, setNoteList] = useState([]);
-  function takeTitle(e) {
-    setTitle(e.target.value);
-  }
-  function takeDes(e) {
-    setDes(e.target.value);
-  }
+  var [editIndex, setEditIndex] = useState(-1);
+  var titleRef = useRef();
+  var desRef = useRef();
+
   function clickHandler() {
+    var title = titleRef.current.value;
+    var des = desRef.current.value;
+    if (title === "" || des === "") return;
     var noteList = [];
     if (localStorage.noteEntry) {
       noteList = JSON.parse(localStorage.noteEntry);
@@ -20,18 +19,27 @@ export default function App() {
     var note = {};
     note.title = title;
     note.des = des;
-    console.log(note);
-    noteList.unshift(note);
-    console.log(noteList);
+    if (editIndex >= 0) {
+      noteList[editIndex].title = note.title;
+      noteList[editIndex].des = note.des;
+      setEditIndex(-1);
+    } else {
+      noteList.unshift(note);
+    }
     localStorage.noteEntry = JSON.stringify(noteList);
     setNoteList(noteList);
+    titleRef.current.value = "";
+    desRef.current.value = "";
   }
+
   function editNote(index) {
     var noteList = [];
     if (localStorage.noteEntry) {
       noteList = JSON.parse(localStorage.noteEntry);
-      console.log(noteList[index]);
+      titleRef.current.value = noteList[index].title;
+      desRef.current.value = noteList[index].des;
     }
+    setEditIndex(index);
   }
   function deleteNote(index) {
     var noteList = [];
@@ -41,6 +49,9 @@ export default function App() {
       localStorage.noteEntry = JSON.stringify(noteList);
       setNoteList(noteList);
     }
+    titleRef.current.value = "";
+    desRef.current.value = "";
+    setEditIndex(-1);
   }
   useEffect(() => {
     var noteList = [];
@@ -49,19 +60,36 @@ export default function App() {
     }
     setNoteList(noteList);
   }, []);
+
+  function searchHandler(e) {
+    var searchText = e.target.value.toLowerCase();
+    var notes = document.getElementsByClassName("row");
+    Array.from(notes).forEach((note) => {
+      var title = note
+        .getElementsByClassName("row-title")[0]
+        .innerText.toLowerCase();
+      if (title.includes(searchText)) {
+        note.classList.remove("hide");
+      } else {
+        note.classList.add("hide");
+      }
+    });
+  }
+
   return (
     <div className="App">
       <h1>Make Notes</h1>
       <input
         type="text"
         name="title"
+        id="title"
         placeholder="TITLE"
-        onChange={takeTitle}
+        ref={titleRef}
       />
       <textarea
         id="description"
         placeholder="DESCRIPTION"
-        onChange={takeDes}
+        ref={desRef}
       ></textarea>
       <button
         onClick={() => {
@@ -70,13 +98,18 @@ export default function App() {
       >
         Add Note
       </button>
-      <input type="search" placeholder="Search Notes" />
+      <input
+        type="search"
+        placeholder="Search Notes By Title"
+        onChange={searchHandler}
+      />
+      <h3>Notes List</h3>
       {noteList.map((note, index) => {
         return (
           <div className="row container">
             <img src={logo} alt="icon" />
             <div className="row-des">
-              <span>{note.title}</span>
+              <div className="row-title">{note.title}</div>
               <small>{note.des}</small>
               <div className="row-control">
                 <button
